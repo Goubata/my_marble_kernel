42a1bf2d06e79e686d098efb3c91f5d1bd3f9692
Rakkashin <101036342+Goubata@users.noreply.github.com>
2025-03-02 22:29:13 +0900

    Update qcom-cpufreq-hw.c

--- kernel/drivers/cpufreq/qcom-cpufreq-hw.c
+++ kernel/drivers/cpufreq/qcom-cpufreq-hw.c
@@ -638,6 +638,10 @@ static int qcom_cpufreq_hw_read_lut(struct platform_device *pdev,
 			c->table[i++].frequency = 2323200;
 		} else if (cpu == 4) {
 			c->table[i++].frequency = 2500000;
+			c->table[i++].frequency = 2572800;
+			c->table[i++].frequency = 2649600;
+			c->table[i++].frequency = 2745600;
+			c->table[i++].frequency = 2865600;
 		} else if (cpu == 7) {
 			c->table[i++].frequency = 3000000;
 			c->table[i++].frequency = 3200200;
